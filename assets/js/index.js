ce78ff654e7f0d36fdb742bd1856770337dda910
Nataliia Breus <breusnataly@gmail.com>
2021-04-08 11:23:04 +0300

    new commit

--- assets/js/index.js
+++ assets/js/index.js
@@ -7,6 +7,7 @@ const capitalize = (str) => {
 }
 console.log(capitalize(str));
 
+
 const sum = (...args) => {
     return args.reduce((accumulator, currentValue)=>{
         if(!Number.isFinite(currentValue)){
@@ -17,3 +18,28 @@ const sum = (...args) => {
 };
 console.log (sum('text', 5, 15, '4'));
 
+const user1 = {
+    name: 'John',
+    surname: 'Smith',
+    age: 21,
+    getFullName(){
+        return `${this.name} ${this.surname}`
+    }
+}
+
+function User(name, surname, age){
+    this.name = name;
+    this.surname = surname;
+    this.age = age;
+    
+}
+
+function UserProto(){
+    this.getFullName = function(){
+        return `${this.name} ${this.surname}`;
+    }
+    this.isAdult = function(){
+        return this.age >= 18;
+    }
+}
+User.prototype = new UserProto();
