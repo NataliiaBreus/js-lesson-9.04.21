ff230e66e300edbe565c916a58a8771fa9e4a59a
Nataliia Breus <breusnataly@gmail.com>
2021-04-08 09:52:42 +0300

    initial commit capitalize throw

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,87 +1,19 @@
-let numA = +prompt("Write a:");
-let numB = +prompt("write b:");
-const operator = prompt("Write operator");
+'use strict';
+const str = "some string i need";
+const capitalize = (str) => {
+    return str.split("").map((word)=>{
+        word[0].toUpperCase() + word.slice(1)
+    }).join("");
+}
+console.log(capitalize(str));
 
-function sum(a, b) {
-  return a + b;
-}
-function sub(a, b) {
-  return a - b;
-}
-function mul(a, b) {
-  return a * b;
-}
-function div(a, b) {
-  return a / b;
-}
-
-const highOrderFunc = function (numA, numB, operationFunc) {
-  const result = operationFunc(numA, numB);
-  console.log(operationFunc);
-  return result;
+const sum = (...args) => {
+    return args.reduce((accumulator, currentValue)=>{
+        if(!Number.isFinite(currentValue)){
+            throw new TypeError ('Wrong argument type');
+        }
+        return accumulator + currentValue;
+    });
 };
-switch (operator) {
-  case "+": {
-    const result = highOrderFunc(numA, numB, sum);
-    console.log(result);
-    break;
-  }
-  case "-": {
-    const result = highOrderFunc(numA, numB, sub);
-    console.log(result);
-    break;
-  }
-  case "*": {
-    const result = highOrderFunc(numA, numB, mul);
-    console.log(result);
-    break;
-  }
-  case "/": {
-    const result = highOrderFunc(numA, numB, div);
-    console.log(result);
-    break;
-  }
-  default: {
-    console.log("Wrong operator");
-    break;
-  }
-}
-
-// Task 2
-
-let counter = 0;
-while (counter <= 10) {
-  
-  if (counter % 2 === 0) {
-    console.log(counter);
-  }
-  counter++;
-}
-console.log("End of loop");
-
-// Task 3 ввод пароля с ограничением попыток до пяти
-
-const GOOD_PASSWORD = "qwerty123";
-let triesCounter = 0;
-while(triesCounter<5){
-    const userPass = prompt ("write your password");
-    if(userPass === GOOD_PASSWORD){
-        console.log ("Success");
-        break;      
-} 
-console.log("you wrote a wrong password!");
-        triesCounter++; 
-}
-
-// Task 4 найти факториал числа
-
-let factCounter = 1;
-const factNumber = +prompt ("Write a number");
-let factorial = 1;
-while(factCounter < factNumber){
-    factCounter ++;
-    factorial *= factCounter;
-}
-console.log(factorial);
- 
+console.log (sum('text', 5, 15, '4'));
 
